f718e82f2780f935f0eea98480b01a4ad88b5ca6
Enhmunh-E <enhuush0704@gmail.com>
2021-12-14 18:50:46 +0800

    ios working added gql

--- src/App.tsx
+++ src/App.tsx
@@ -11,57 +11,77 @@ import Colors from './styles/colors';
 import Home from './screens/Home';
 import HomeHeader from './components/HomeHeader';
 import {Provider} from './provider/provider';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+const httpLink = createHttpLink({
+  fetch,
+  uri: 'https://graphql.contentful.com/content/v1/spaces/y0vql4lgwa04',
+  headers: {
+    Authorization: 'Bearer q9eJqpPqdgKU4wfjztDngADtDij5aFcMiY41xo-YX3c',
+  },
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 const Stack = createNativeStackNavigator<RootStackParamList>();
 const App = () => {
   return (
-    <NavigationContainer>
-      <Provider>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Welcome"
-            component={Welcome}
-            options={{
-              headerTitle: () => <LetsGo isdark={true} />,
-              headerStyle: {
-                backgroundColor: 'transparent',
-              },
-              headerTransparent: true,
-              headerShadowVisible: false,
-            }}
-          />
-          <Stack.Screen
-            name="Welcome2"
-            component={Welcome2}
-            options={{
-              headerTitle: () => <LetsGo isdark={false} />,
-              headerRight: () => (
-                <View
-                  style={{
-                    height: 40,
-                    width: 40,
-                    backgroundColor: Colors.primary,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    borderRadius: 20,
-                  }}>
-                  <SearchIcon />
-                </View>
-              ),
-              headerTransparent: true,
-              headerShadowVisible: false,
-              headerBackVisible: false,
-            }}
-          />
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{
-              header: () => <HomeHeader />,
-            }}
-          />
-        </Stack.Navigator>
-      </Provider>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Provider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Welcome"
+              component={Welcome}
+              options={{
+                headerTitle: () => <LetsGo isdark={true} />,
+                headerStyle: {
+                  backgroundColor: 'transparent',
+                },
+                headerTransparent: true,
+                headerShadowVisible: false,
+              }}
+            />
+            <Stack.Screen
+              name="Welcome2"
+              component={Welcome2}
+              options={{
+                headerTitle: () => <LetsGo isdark={false} />,
+                headerRight: () => (
+                  <View
+                    style={{
+                      height: 40,
+                      width: 40,
+                      backgroundColor: Colors.primary,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      borderRadius: 20,
+                    }}>
+                    <SearchIcon />
+                  </View>
+                ),
+                headerTransparent: true,
+                headerShadowVisible: false,
+                headerBackVisible: false,
+              }}
+            />
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{
+                header: () => <HomeHeader />,
+              }}
+            />
+          </Stack.Navigator>
+        </Provider>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 };
 export default App;
--- src/components/HomeCarousel.tsx
+++ src/components/HomeCarousel.tsx
@@ -4,71 +4,39 @@ import {
   ImageBackground,
   NativeScrollEvent,
   NativeSyntheticEvent,
+  StyleSheet,
   useWindowDimensions,
   View,
+  Text,
 } from 'react-native';
+import {BlurView} from '@react-native-community/blur';
 import LinearGradient from 'react-native-linear-gradient';
-export const HomeCarousel = () => {
+import MapIcon from '../assets/treasure-map-line.svg';
+import Colors from '../styles/colors';
+import Star from '../assets/star-fill.svg';
+import Clock from '../assets/time-line.svg';
+
+type ActivityType = {
+  title: string;
+  rate: number;
+  category: {
+    name: string;
+  };
+  image: {
+    url: string;
+  };
+  sys: {
+    id: string;
+  };
+};
+
+type Props = {
+  data: ActivityType[];
+};
+export const HomeCarousel = ({data}: Props) => {
+  console.log(data);
   const [imageIndex, setImageIndex] = useState(0);
   const {width} = useWindowDimensions();
-  const data1 = [
-    {
-      id: 1,
-      name: 'centraal',
-      about: 'Taking a boat tour through Canals',
-      star: 4.1,
-      closest: '7.35pm',
-      image: require('../assets/centraal.png'),
-    },
-    {
-      id: 2,
-      name: 'centraal',
-      about: 'Taking a boat tour through Canals',
-      star: 4.1,
-      closest: '7.35pm',
-      image: require('../assets/centraal.png'),
-    },
-    {
-      id: 3,
-      name: 'centraal',
-      about: 'Taking a boat tour through Canals',
-      star: 4.1,
-      closest: '7.35pm',
-      image: require('../assets/centraal.png'),
-    },
-    {
-      id: 4,
-      name: 'centraal',
-      about: 'Taking a boat tour through Canals',
-      star: 4.1,
-      closest: '7.35pm',
-      image: require('../assets/centraal.png'),
-    },
-    {
-      id: 5,
-      name: 'centraal',
-      about: 'Taking a boat tour through Canals',
-      star: 4.1,
-      closest: '7.35pm',
-      image: require('../assets/centraal.png'),
-    },
-    {
-      id: 6,
-      name: 'centraal',
-      about: 'Taking a boat tour through Canals',
-      star: 4.1,
-      closest: '7.35pm',
-      image: require('../assets/centraal.png'),
-    },
-    {
-      id: 7,
-      name: 'centraal',
-      about: 'Taking a boat tour through Canals',
-      star: 4.1,
-      closest: '7.35pm',
-      image: require('../assets/centraal.png'),
-    },
-  ];
   const onScroll = useCallback(
     (event: NativeSyntheticEvent<NativeScrollEvent>) => {
       const slideSize = event.nativeEvent.layoutMeasurement.width;
@@ -83,13 +51,22 @@ export const HomeCarousel = () => {
       style={{
         height: ((width - 48) * 213) / 327,
         width: width - 48,
+        marginTop: 16,
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 12,
+        },
+        shadowOpacity: 0.58,
+        shadowRadius: 16.0,
+        elevation: 24,
       }}>
       <FlatList
-        data={data1}
+        data={data}
         style={{
           borderRadius: 5,
         }}
-        keyExtractor={item => '' + item.id}
+        keyExtractor={item => item.sys.id}
         pagingEnabled={true}
         horizontal={true}
         onScroll={onScroll}
@@ -97,12 +74,14 @@ export const HomeCarousel = () => {
         showsHorizontalScrollIndicator={false}
         renderItem={({item}) => (
           <ImageBackground
-            source={item.image}
-            resizeMode="repeat"
+            // key={item.id}
+            source={{uri: item.image.url}}
+            resizeMode="cover"
             style={{
               height: ((width - 48) * 213) / 327,
               width: width - 48,
               borderRadius: 10,
+              padding: 16,
             }}>
             <LinearGradient
               colors={['rgba(54, 54, 54, 0)', 'rgba(0, 0, 0, 0.8)']}
@@ -112,6 +91,37 @@ export const HomeCarousel = () => {
                 height: ((width - 48) * 213) / (327 * 2),
                 width: width - 48,
               }}></LinearGradient>
+            <View style={[styles.col, styles.between, {flex: 1}]}>
+              <View style={[styles.row, styles.between]}>
+                <View style={[styles.itemNameContainer, styles.center]}>
+                  <MapIcon />
+                  <Text style={{color: '#D27C4A', paddingLeft: 2}}>
+                    centraal
+                  </Text>
+                </View>
+                <View style={styles.col}>
+                  <BlurView
+                    blurType="ultraThinMaterialDark"
+                    blurAmount={4}
+                    style={[styles.row, styles.center, styles.starContainer]}>
+                    <Star />
+                    <Text style={{color: Colors.primary, paddingLeft: 4}}>
+                      {item.rate}
+                    </Text>
+                  </BlurView>
+                  <View style={[styles.closestContainer, styles.center]}>
+                    <Clock />
+                    <Text style={{color: Colors.primary, fontSize: 12}}>
+                      Closest
+                    </Text>
+                    <Text style={{color: Colors.primary, fontSize: 12}}>
+                      7.35pm
+                    </Text>
+                  </View>
+                </View>
+              </View>
+              <Text style={styles.about}>{item.title}</Text>
+            </View>
           </ImageBackground>
         )}
       />
@@ -124,10 +134,12 @@ export const HomeCarousel = () => {
           display: 'flex',
           flexDirection: 'row',
         }}>
-        {data1.map((dt, index) => (
+        {data.map((dt, index) => (
           <View
+            key={index}
             style={{
-              width: 6,
+              opacity: 1 - (1 / data.length) * Math.abs(imageIndex - index),
+              width: imageIndex == index ? 25 : 6,
               height: 6,
               backgroundColor: 'white',
               borderRadius: 6,
@@ -138,4 +150,103 @@ export const HomeCarousel = () => {
     </View>
   );
 };
+const styles = StyleSheet.create({
+  itemNameContainer: {
+    width: 79,
+    height: 22,
+    backgroundColor: '#FFF0D8',
+    borderRadius: 11,
+    flexDirection: 'row',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  col: {
+    flexDirection: 'column',
+  },
+  center: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  about: {
+    fontSize: 28,
+    lineHeight: 33,
+    color: Colors.primary,
+    paddingBottom: 13,
+  },
+  between: {
+    justifyContent: 'space-between',
+  },
+  starContainer: {
+    width: 56,
+    paddingHorizontal: 6,
+    paddingVertical: 4,
+    borderRadius: 12,
+  },
+  closestContainer: {
+    width: 56,
+    height: 62,
+    backgroundColor: Colors.secondary,
+    marginTop: 8,
+    borderRadius: 7,
+  },
+});
 export default HomeCarousel;
+// const data1 = [
+//   {
+//     id: '1',
+//     name: 'centraal',
+//     about: 'Taking a boat tour through Canals',
+//     star: 4.1,
+//     closest: '7.35pm',
+//     image: require('../assets/centraal.png'),
+//   },
+//   {
+//     id: '2',
+//     name: 'centraal',
+//     about: 'Taking a boat tour through Canals',
+//     star: 4.1,
+//     closest: '7.35pm',
+//     image: require('../assets/centraal.png'),
+//   },
+//   {
+//     id: '3',
+//     name: 'centraal',
+//     about: 'Taking a boat tour through Canals',
+//     star: 4.1,
+//     closest: '7.35pm',
+//     image: require('../assets/centraal.png'),
+//   },
+//   {
+//     id: '4',
+//     name: 'centraal',
+//     about: 'Taking a boat tour through Canals',
+//     star: 4.1,
+//     closest: '7.35pm',
+//     image: require('../assets/centraal.png'),
+//   },
+//   {
+//     id: '5',
+//     name: 'centraal',
+//     about: 'Taking a boat tour through Canals',
+//     star: 4.1,
+//     closest: '7.35pm',
+//     image: require('../assets/centraal.png'),
+//   },
+//   {
+//     id: '6',
+//     name: 'centraal',
+//     about: 'Taking a boat tour through Canals',
+//     star: 4.1,
+//     closest: '7.35pm',
+//     image: require('../assets/centraal.png'),
+//   },
+//   {
+//     id: '7',
+//     name: 'centraal',
+//     about: 'Taking a boat tour through Canals',
+//     star: 4.1,
+//     closest: '7.35pm',
+//     image: require('../assets/centraal.png'),
+//   },
+// ];
